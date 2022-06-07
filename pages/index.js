35d05fb7a4ae2ac0b9d8e4cebbd76e672974ded4
madeleineenberg <madeleine_enberg@hotmail.com>
2022-06-07 14:24:08 +0200

    add structure components

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import Navbar from '../components/Navbar';
+import ResorceHighlight from '../components/ResorceHighlight';
+import Newsletter from '../components/Newsletter';
+import ResourceList from '../components/ResourceList';
+import Footer from '../components/Footer';
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <Navbar />
+      <ResorceHighlight />
+      <Newsletter />
+      <ResourceList />
+      <Footer />
+    </>
+  );
 }
