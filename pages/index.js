8d20181236df5021ded7a7a1d657fa783413b501
madeleineenberg <madeleine_enberg@hotmail.com>
2022-06-08 20:56:56 +0200

    fetch data from node api

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,37 @@
-import Navbar from '../components/Navbar';
-import ResorceHighlight from '../components/ResorceHighlight';
-import Newsletter from '../components/Newsletter';
-import ResourceList from '../components/ResourceList';
-import Footer from '../components/Footer';
+import Navbar from 'components/Navbar';
+import ResorceHighlight from 'components/ResorceHighlight';
+import Newsletter from 'components/Newsletter';
+import ResourceList from 'components/ResourceList';
+import Footer from 'components/Footer';
+import Layout from 'components/Layout';
 
-export default function Home() {
+
+
+export default function Home({ resources }) {
+
   return (
-    <>
-      <Navbar />
-      <ResorceHighlight />
+    <Layout>
+      <ResorceHighlight resources={resources.slice(0, 2)} />
       <Newsletter />
-      <ResourceList />
+      <ResourceList resources={resources.slice(2)} />
       <Footer />
-    </>
+    </Layout>
   );
 }
+
+export async function getServerSideProps() {
+  // is called every time you will visit the page
+  // function is executed on the server
+  // data are always fresh
+
+  const resData = await fetch('http://localhost:3001/api/resources');
+  const data = await resData.json();
+ 
+  return {
+    props: {
+      resources: data,
+    },
+  };
+}
+
+
