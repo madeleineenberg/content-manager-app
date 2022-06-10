f945f38b4e8f8decfa5c18f196b1713ea7148bdd
madeleineenberg <madeleine_enberg@hotmail.com>
2022-06-10 16:27:09 +0200

    fetch resources proxy server

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,8 +1,29 @@
-
+import axios from 'axios';
 
 export default async function (req, res) {
- const dataRes = await fetch('http://localhost:3001/api/resources');
- const data = await dataRes.json()
+  if (req.method === 'GET') {
+    const dataRes = await fetch('http://localhost:3001/api/resources');
+    const data = await dataRes.json();
 
- res.send(data);
+    return res.send(data);
+  }
+
+  if (req.method === 'POST') {
+    console.log(req.body);
+    const { title, description, link, timeToFinish, priority } = req.body;
+
+    if (!title || !description || !link || !timeToFinish || !priority) {
+      return res.status(422).send('data is missing');
+    }
+
+    try {
+      const axiosRes = await axios.post(
+        'http://localhost:3001/api/resources',
+        req.body
+      );
+      return res.send(axiosRes.data);
+    } catch {
+      return status(422).send('Data cannot be stored');
+    }
+  }
 }
