05de20a45858813e689ab501f0f36e9f5315477c
madeleineenberg <madeleine_enberg@hotmail.com>
2022-06-10 20:53:55 +0200

    add: resource form component

---
+++ components/ResourceForm.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+
+const DEFAULT_DATA = {
+  title: '',
+  description: '',
+  link: '',
+  priority: '2',
+  timeToFinish: 60,
+};
+
+export default function ResourceForm({ onFormSubmit }) {
+  const [form, setForm] = useState(DEFAULT_DATA);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const submitForm = () => {
+    onFormSubmit(form);
+  };
+
+  return (
+    <div className='resource-form'>
+      <h1 className='title'>Add New Resource</h1>
+      <form>
+        <div className='field'>
+          <label className='label'>Title</label>
+          <div className='control'>
+            <input
+              value={form.title}
+              name='title'
+              onChange={handleChange}
+              className='input'
+              type='text'
+              placeholder='Learn something'
+            />
+          </div>
+        </div>
+        <div className='field'>
+          <label className='label'>Description</label>
+          <div className='control'>
+            <textarea
+              value={form.description}
+              onChange={handleChange}
+              name='description'
+              className='textarea'
+              placeholder='Learn these technologies because...'
+            ></textarea>
+          </div>
+        </div>
+        <div className='field'>
+          <label className='label'>Link</label>
+          <div className='control'>
+            <input
+              value={form.link}
+              onChange={handleChange}
+              name='link'
+              className='input'
+              type='text'
+              placeholder='link to docs'
+            />
+          </div>
+        </div>
+        <div className='field'>
+          <label className='label'>Priority</label>
+          <div className='control'>
+            <div className='select'>
+              <select
+                value={form.priority}
+                onChange={handleChange}
+                name='priority'
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className='field'>
+          <label className='label'>Time to finish</label>
+          <div className='control'>
+            <input
+              value={form.timeToFinish}
+              onChange={handleChange}
+              name='timeToFinish'
+              className='input'
+              type='number'
+              placeholder='60'
+            />
+          </div>
+          <p className='help'>Time is in minutes</p>
+        </div>
+        <div className='field is-grouped'>
+          <div className='control'>
+            <button
+              type='button'
+              onClick={submitForm}
+              className='button is-link'
+            >
+              Submit
+            </button>
+          </div>
+          <div className='control'>
+            <button
+              type='button'
+              onClick={() => setForm(DEFAULT_DATA)}
+              className='button is-link is-light'
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
--- pages/resources/new.js
+++ pages/resources/new.js
@@ -1,23 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Layout from 'components/Layout';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-
-const DEFAULT_DATA = {
-  title: '',
-  description: '',
-  link: '',
-  priority: '2',
-  timeToFinish: '60',
-};
+import ResourceForm from 'components/ResourceForm';
 
 export default function ResourcesCreate() {
-  const [form, setForm] = useState(DEFAULT_DATA);
   const router = useRouter();
 
-  const submitForm = () => {
+  const createResource = (formData) => {
     axios
-      .post('/api/resources', form)
+      .post('/api/resources', formData)
       .then(() => {
         router.push('/');
       })
@@ -26,112 +18,12 @@ export default function ResourcesCreate() {
       });
   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
   return (
     <Layout>
       <div className='container'>
         <div className='columns'>
           <div className='column is-8 is-offset-2'>
-            <div className='resource-form'>
-              <h1 className='title'>Add New Resource</h1>
-              <form>
-                <div className='field'>
-                  <label className='label'>Title</label>
-                  <div className='control'>
-                    <input
-                      value={form.title}
-                      name='title'
-                      onChange={handleChange}
-                      className='input'
-                      type='text'
-                      placeholder='Learn something'
-                    />
-                  </div>
-                </div>
-                <div className='field'>
-                  <label className='label'>Description</label>
-                  <div className='control'>
-                    <textarea
-                      value={form.description}
-                      onChange={handleChange}
-                      name='description'
-                      className='textarea'
-                      placeholder='Learn these technologies because...'
-                    ></textarea>
-                  </div>
-                </div>
-                <div className='field'>
-                  <label className='label'>Link</label>
-                  <div className='control'>
-                    <input
-                      value={form.link}
-                      onChange={handleChange}
-                      name='link'
-                      className='input'
-                      type='text'
-                      placeholder='link to docs'
-                    />
-                  </div>
-                </div>
-                <div className='field'>
-                  <label className='label'>Priority</label>
-                  <div className='control'>
-                    <div className='select'>
-                      <select
-                        value={form.priority}
-                        onChange={handleChange}
-                        name='priority'
-                      >
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-                <div className='field'>
-                  <label className='label'>Time to finish</label>
-                  <div className='control'>
-                    <input
-                      value={form.timeToFinish}
-                      onChange={handleChange}
-                      name='timeToFinish'
-                      className='input'
-                      type='number'
-                      placeholder='60'
-                    />
-                  </div>
-                  <p className='help'>Time is in minutes</p>
-                </div>
-                <div className='field is-grouped'>
-                  <div className='control'>
-                    <button
-                      type='button'
-                      onClick={submitForm}
-                      className='button is-link'
-                    >
-                      Submit
-                    </button>
-                  </div>
-                  <div className='control'>
-                    <button
-                      type='button'
-                      onClick={() => setForm(DEFAULT_DATA)}
-                      className='button is-link is-light'
-                    >
-                      Reset
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
+            <ResourceForm onFormSubmit={createResource} />
           </div>
         </div>
       </div>
