9469cd5066f87a1ea20dce18dfc2ddd7ce9660b3
madeleineenberg <madeleine_enberg@hotmail.com>
2022-06-14 17:00:54 +0200

    add: Resource state label

---
+++ components/ResourceLabel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ResourceLabel({status}) {
+  return (
+    <span className={`tag is-large ml-4 resource-${status}`}>
+        {status}
+    </span>
+  )
+}
